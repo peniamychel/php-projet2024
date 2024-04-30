215e2c56d89194154a66e19f8b27868551081b78
maykad7 <mayk.d7@gmail.com>
2024-04-30 19:41:53 -0400

    Probando en repositorio

--- admin/db.php
+++ admin/db.php
@@ -1,8 +1,13 @@
 <?php
-$servidor ="localhost";
-$basedatos = "website";
-$usuario = "root";
-$contrasenia = "";
+// $servidor ="localhost";
+// $basedatos = "website";
+// $usuario = "root";
+// $contrasenia = "";
+
+$servidor ="bmmgi9g9t8xkbjnwaifm-mysql.services.clever-cloud.com";
+$basedatos = "bmmgi9g9t8xkbjnwaifm";
+$usuario = "uq3od9bg3tpktkqt";
+$contrasenia = "lTUml9YowyvMiFlQi4Mj";
 
 try {
     $conexion = new PDO("mysql:host=$servidor;dbname=$basedatos", $usuario, $contrasenia);
